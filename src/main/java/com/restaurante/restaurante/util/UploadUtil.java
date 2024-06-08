a78952f48e4f86f96096cea211fbb67435de6d20
Nanda P Caetano <fernandamariacaetanopereira@gmail.com>
2024-06-08 12:37:41 -0300

    Update UploadUtil.java

--- src/main/java/com/restaurante/restaurante/util/UploadUtil.java
+++ src/main/java/com/restaurante/restaurante/util/UploadUtil.java
@@ -3,17 +3,24 @@ package com.restaurante.restaurante.util;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+@Component
 public class UploadUtil {
-    public static boolean fazerUploadImagem(MultipartFile imagem) {
+
+    @Value("${upload.image.directory}")
+    private String pastaUploadImagem;
+
+    public boolean fazerUploadImagem(MultipartFile imagem) {
         boolean sucessoUpload = false;
         if (!imagem.isEmpty()) {
             String nomeArquivo = imagem.getOriginalFilename();
             try {
                 // Criando diretório para armazenar o arquivo
-                String pastaUploadImagem = "C:\\Users\\Marili\\Desktop\\Nanda\\FATEC\\Desenvolvimento para Servidores I\\RestauranteV2\\restaurante\\src\\main\\resources\\static\\images\\pratos";
                 File dir = new File(pastaUploadImagem);
                 if (!dir.exists()) {
                     dir.mkdirs();
@@ -21,10 +28,12 @@ public class UploadUtil {
 
                 // Criando o arquivo no diretório
                 File serverFile = new File(dir.getAbsolutePath() + File.separator + nomeArquivo);
-                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
-                stream.write(imagem.getBytes());
-                stream.close();
-                sucessoUpload = true;
+                try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile))) {
+                    stream.write(imagem.getBytes());
+                    sucessoUpload = true;
+                } catch (IOException e) {
+                    System.out.println("Você falhou! " + e.getMessage());
+                }
             } catch (Exception e) {
                 System.out.println("Você falhou! " + e.getMessage());
             }
